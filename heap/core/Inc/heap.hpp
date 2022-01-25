0e9be880c17a4e72403166fd78d91a09745c24ab
grkmkctrk <gkocaturk54@gmail.com>
2022-01-25 15:39:19 +0300

    tired

--- heap/core/Inc/heap.hpp
+++ heap/core/Inc/heap.hpp
@@ -14,7 +14,7 @@ enum class HeapType{ // to reach from main created as global class
 };
 
 // Heap class
-template <typename T>
+template <class T>
 class Heap {
 private:
 
@@ -48,8 +48,9 @@ private:
 
     // Insertion
     void insertMin(T value);
-    void insertMax(T value);
+    // void insertMax(T value);
     void swap(HeapNode *a, HeapNode *b);
+    void sortIt(int8_t deep);
 
 public:
     // Constructor 
@@ -63,8 +64,8 @@ public:
 
     // Heap operations
     void insert(T value);
-    void remove(T value);
-    void printNode();   
+    // void remove(T value);
+    auto printNode(uint8_t deep, uint64_t index);   
     void printHeap();
 };
 
@@ -107,25 +108,22 @@ void Heap<T>::insertMin(T value){
 
     if(isEmpty()){
         root = new HeapNode(value);
-        inx++;
         return;
     }
 
+    
     while(go){
-
         if(tempDepth == 1){            
             if(inx % 2 == 0){
-                if(curr->left == nullptr){
-                    curr->left = new HeapNode(value, curr);
-                    inx++;
+                if(curr->right == nullptr){
+                    curr->right = new HeapNode(value, curr);
                 }
                 go = false;
             }else{
 
-                if(curr->left != nullptr)
-                    std::cout << curr->left->data << " ";
-                else std::cout << " ";
-                
+                if(curr->left == nullptr){
+                    curr->left = new HeapNode(value, curr);
+                }
                 go = false;
             }
             inx++;
@@ -150,9 +148,92 @@ void Heap<T>::insertMin(T value){
     }
     tempInx = inx;
     tempDepth = depth;
+
+    if(depth > 1){
+        sortIt(depth-1);
+    } 
 }
 
+template<class T>
+void Heap<T>::sortIt(int8_t deep){
+    // go deep time left
+    // HeapNode *curr = printNode(deep, 1);
 
+    for(size_t index = 1; index < pow(2, deep); index++){
+        auto now = printNode(deep, index);
+        auto next = printNode(deep, index+1);
+        if(now->value > next->value){
+            swap(now, next);
+        }
+    }
+
+
+}
+
+template<class T>
+auto Heap<T>::printNode(uint8_t deep, uint64_t index){
+    bool go = true;
+    uint64_t tempIndex = index;
+    HeapNode* curr = root;
+
+    if(index > pow(2, deep)){
+        std::cout << "Error" << std::endl;
+        throw std::runtime_error("Index out of range");
+    }else if(!deep){
+        // return curr->value;
+        return curr;
+    }else{
+        while(go){
+        
+            if(deep == 1){
+                if(index % 2 == 0){
+                    if(curr->right != nullptr){
+                        // std::cout << curr->right->value << " ";
+                        // return curr->right->value;
+                        return curr->right;
+                    }
+                    go = false;
+                }else{
+                    if(curr->left != nullptr){
+                        // std::cout << curr->left->value << " ";
+                        // return curr->left->value;
+                        return curr->left;
+                    }
+                    go = false;
+                }
+            }
+
+            if(deep > 1){
+                uint16_t side = pow(2, deep)/2; // should i go left or right?
+                if(side < tempIndex){
+                    curr = curr->right;
+                    tempIndex = tempIndex - side;
+                }
+                else curr = curr->left;
+                deep--;
+            }
+        }
+    }
+    return root;
+}
+
+template<class T>
+void Heap<T>::printHeap(){
+
+    if(isEmpty()){
+        std::cout << "Empty" << std::endl;
+        return;
+    }else{
+        uint8_t deptH = (inx == 1) ? (depth-1) : (depth);
+        for(size_t d = 0; d <= deptH; d++){
+            for(size_t leaf = 1; leaf <= pow(2, d); leaf++){
+                // std::cout << d << '-' << inx << std::endl; 
+                std::cout << printNode(d, leaf)->value << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
+}
 
 template<class T>
 void Heap<T>::swap(HeapNode *a, HeapNode *b){
@@ -160,5 +241,6 @@ void Heap<T>::swap(HeapNode *a, HeapNode *b){
     a->value = b->value;
     b->value = temp;
 }
+
 
 #endif
--- heap/core/Src/main.cpp
+++ heap/core/Src/main.cpp
@@ -6,5 +6,26 @@ int main(){
 
     h.insert(5);
 
+    h.insert(28);
+    h.insert(16);
+    
+    h.insert(28);
+    h.insert(20);
+    h.insert(34);
+    h.insert(27);
+
+    h.insert(42);
+    h.insert(59);
+    h.insert(63);
+    h.insert(60);
+    h.insert(72);
+    h.insert(67);
+    h.insert(70);
+    h.insert(71);
+
+
+    h.printHeap();
+
+
     return 0;
 }
